7f8854aedfca636fb25199cbc9546b3214827baf
unknown <bendanamarkderrick@gmail.com>
2022-10-01 16:18:41 +0800

    added login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './auth/auth-guard.service';
+import { LoginGuardService } from './auth/login-guard.service';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { TodosListComponent } from './components/todos-list/todos-list.component';
 
 const routes: Routes = [
   {
     path: "login",
-    component: LoginRegisterComponent
+    component: LoginRegisterComponent,
+    canActivate: [LoginGuardService]
   },
   {
     path: "",
---
+++ src/app/auth/login-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardService {
+
+  constructor(public auth: AuthService, public router: Router) { }
+
+  canActivate(): boolean {
+    if (this.auth.isLoggedIn()) {
+      this.router.navigate(['/']);
+      return false;
+    }
+    return true;
+  }
+}
